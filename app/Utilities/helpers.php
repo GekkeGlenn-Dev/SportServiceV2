277edad05767a51b961e6a5871ffbfbb4ffde2ad
GekkeGlenn-Dev <glenn.dev@hotmail.com>
2021-07-07 00:35:38 +0200

    Improved helpers.php.

--- app/Utilities/helpers.php
+++ app/Utilities/helpers.php
@@ -1,26 +1,48 @@
 <?php
 
+use App\Models\Options;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 if (!function_exists('isOptionDisabled')) {
 
-    function isOptionEnabled($key): bool {
+    /**
+     * Check with the given key if the option is enabled
+     *
+     * @param string $key
+     * @return bool If enabled it will return TRUE otherwise FALSE
+     */
+    function isOptionEnabled(string $key): bool
+    {
         $data = getOptions($key);
-        foreach($data as $item) {
+        foreach ($data as $item) {
             if ($item->value == 1) {
                 return true;
             }
-
-            return false;
         }
+
+        return false;
     }
 }
 
 if (!function_exists('getOptions')) {
 
-    // Document todo -> collection return??
-    function getOptions($key): Collection {
-        return DB::table('options')->select('key', 'value')->where('key', '=', $key)->get();
+    /**
+     * Get an option from the database table
+     *
+     * @param string $key
+     * @param string $database
+     * @return Collection
+     */
+    function getOptions(string $key, string $database = 'options'): Collection
+    {
+        if ($database != 'options') {
+            return DB::table($database)
+                ->select('key', 'value')
+                ->where('key', '=', $key)
+                ->get();
+        }
+
+        return Options::where('key', '=', $key)->get(['key', 'value']);
     }
 }
